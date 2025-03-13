3ffd310a161a11bc4917de7d43a2ee85592cc7db
Nafees Ahmad <138881808+Nafees72@users.noreply.github.com>
2025-03-13 12:11:34 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,71 @@
+// let todo = [];
+// let req = prompt("Please enter the request you want to add");
+// while(true){
+//     if(req == "quit"){
+//         console.log("quit the app");
+//         break;
+//     }
+//     else if(req == "list"){
+//         for(let i=0;i<todo.length;i++){
+//              console.log(i,todo[i]);
+//         }
+
+//         }
+//         else if(req == "add"){
+//             let info = prompt("Enter the request you want to add");
+//             todo.push(info);
+//     }
+//     else if(req == "delete"){
+//         let idx = prompt("What you want to delete");
+//         todo.splice(idx,1);
+//         console.log("Todo deleted");
+//     }
+//     req = prompt("Enter the request");
+// }
+// let todo = [];
+// let req = prompt("Please enter the request you want to perform");
+// while(true){
+//     if(req == "quit"){
+//         console.log("quit the app");
+//         break;
+//     }
+//     else if(req == "list"){
+//         for(let i=0;i<todo.length;i++){
+//             console.log(i,todo[i]);
+//         }
+//         }
+//         if(req == "add"){
+//             let task = prompt("Please enter the task you want to add");
+//             todo.push(task);
+//         }
+//         else if(req == "delete"){
+//             let idx = prompt("Which task ypu want to delete");
+//             todo.splice(idx,1);
+//             console.log("Task deleted");
+//         }
+//     req = prompt("Enter the request");
+// }
+
+// const sum =(a,b)=>{
+//     console.log(a+b);
+// }
+// sum(10,20);
+
+// function isadult(age){
+//     if(age>18){
+//         console.log("Elligible to vote");
+//     }
+//     else{
+//         console.log("Not elligible");
+//     }
+// }
+// isadult(19);
+
+// const mul = (a,b)=> (a*b);
+
+// console.log("hii");
+
+// setInterval(() => {
+//     console.log("This is");
+// }, 2000);
+// console.log("Nafees Ahmad here");
